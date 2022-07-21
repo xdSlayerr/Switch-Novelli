df5a6e540426fedb314af618cedc9daed762333d
Lautaro E. Novelli <l.novelli@outlook.com>
2022-07-21 09:27:01 -0300

    ?

---
+++ navigation/navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+ const ShopNavigator = () => {
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName= "Home">
+            <Stack.Screen name = "" component={}/>
+            <Stack.Screen name = "" component={}/>
+            <Stack.Screen name = "" component={}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+ };
+
+
+ export default ShopNavigator;
